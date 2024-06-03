2d69b204e0d9cc6cb4372c81026c03ba1687692d
Sydney Deb <sydneyd30@nycstudents.net>
2024-06-03 09:30:32 -0400

    added files

--- src/Game.java
+++ src/Game.java
@@ -9,10 +9,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 public class Game extends JFrame implements MouseListener, ActionListener {
     private JPanel frame;
@@ -29,27 +26,32 @@ public class Game extends JFrame implements MouseListener, ActionListener {
         loadUI();
         resetGame();
         addMouseListener(this);
-        time = 60;
+        time = 30;
         timer = new Timer(1000, this);
         start.addActionListener(this);
     }
 
     public void mouseClicked(MouseEvent e) {
-        JLabel lbl = (JLabel)e.getSource();
-        int id = Integer.parseInt(lbl.getName());
-        clicked(id);
     }
     public void endGame() {
         JOptionPane.showMessageDialog(this, "Your Scored " + score + " points!","Game Over!", JOptionPane.INFORMATION_MESSAGE);
         score = 0;
-        time = 60;
-        timeText.setText("60");
+        time = 30;
+        timeText.setText("30");
         scoreText.setText("Score: 0");
         resetGame();
     }
     public void mousePressed(MouseEvent e) {
     }
     public void mouseReleased(MouseEvent e) {
+        score++;
+        System.out.println(score);
+        scoreText.setText("Score: " + score);
+        resetGame();
+        spawnMole();
+        JLabel lbl = (JLabel)e.getSource();
+        int id = Integer.parseInt(lbl.getName());
+        clicked(id);
     }
     public void mouseEntered(MouseEvent e) {
     }
@@ -96,7 +98,7 @@ public class Game extends JFrame implements MouseListener, ActionListener {
         scoreText.setBounds(25, 10, 600, 50);
         game.add(scoreText);
 
-        timeText = new JLabel("Time: 60");
+        timeText = new JLabel("Time: 30");
         timeText.setHorizontalAlignment(SwingConstants.CENTER);
         timeText.setForeground(new Color(240, 128, 128));
         timeText.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
@@ -118,74 +120,75 @@ public class Game extends JFrame implements MouseListener, ActionListener {
         holes[0] = new JLabel("0");
         holes[0].setName("0");
         holes[0].setBounds(0, 400, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[0]);
 
         holes[1] = new JLabel("1");
         holes[1].setName("1");
         holes[1].setBounds(135, 400, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[1]);
 
         holes[2] = new JLabel("2");
         holes[2].setName("2");
         holes[2].setBounds(270, 400, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[2]);
 
-        holes[3] = new JLabel("4");
-        holes[3].setName("4");
+        holes[3] = new JLabel("3");
+        holes[3].setName("3");
         holes[3].setBounds(0, 270, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[3]);
 
-        holes[4] = new JLabel("5");
-        holes[4].setName("5");
+        holes[4] = new JLabel("4");
+        holes[4].setName("4");
         holes[4].setBounds(135, 270, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[4]);
 
-        holes[5] = new JLabel("6");
-        holes[5].setName("6");
+        holes[5] = new JLabel("5");
+        holes[5].setName("5");
         holes[5].setBounds(270, 270, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[5]);
 
-        holes[6] = new JLabel("8");
-        holes[6].setName("8");
+        holes[6] = new JLabel("6");
+        holes[6].setName("6");
         holes[6].setBounds(0, 135, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[6]);
 
-        holes[7] = new JLabel("9");
-        holes[7].setName("9");
+        holes[7] = new JLabel("7");
+        holes[7].setName("7");
         holes[7].setBounds(135, 135, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[7]);
 
-        holes[8] = new JLabel("10");
-        holes[8].setName("10");
+        holes[8] = new JLabel("8");
+        holes[8].setName("8");
         holes[8].setBounds(270, 135, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[8]);
 
-        holes[9] = new JLabel("12");
-        holes[9].setName("12");
+        holes[9] = new JLabel("9");
+        holes[9].setName("9");
         holes[9].setBounds(0, 0, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[9]);
 
-        holes[10] = new JLabel("13");
-        holes[10].setName("13");
+        holes[10] = new JLabel("10");
+        holes[10].setName("10");
         holes[10].setBounds(135, 0, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[10]);
 
-        holes[11] = new JLabel("14");
-        holes[11].setName("14");
+        holes[11] = new JLabel("11");
+        holes[11].setName("11");
         holes[11].setBounds(270, 0, 132, 132);
-        holes[0].addMouseListener(this);
         frame.add(holes[11]);
+
+        for(int i = 0; i < 11; i++) {
+            final int p = i;
+            holes[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (e.getSource() == holes[p]) {
+                        int id = Integer.parseInt(holes[p].getName());
+                        clicked(id);
+                    }
+                }
+            });
+        }
         setContentPane(game);
     }
 
